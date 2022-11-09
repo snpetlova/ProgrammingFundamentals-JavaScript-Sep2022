bc5d6995ac16505aee6375ea12623903f2e178dd
Snezhana Petlova <snpetlova@gmail.com>
2022-11-09 10:16:33 +0200

    -

--- 6-Objects and Classes/convertToObject.js
+++
@@ -1,9 +0,0 @@
-function convertToObject(jsonString) {
-
-    let personInfo = JSON.parse(jsonString);
-
-    for (const key of Object.keys(personInfo)) {
-        console.log(`${key}: ${personInfo[key]}`);
-    }
-
-}convertToObject('{"name": "George", "age": 40, "town": "Sofia"}')
