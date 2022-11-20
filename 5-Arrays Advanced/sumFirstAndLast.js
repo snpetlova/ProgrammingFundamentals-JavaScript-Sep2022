ee344a2c9e99862520789c401f23a132515a013f
Snezhana Petlova <snpetlova@gmail.com>
2022-11-20 13:35:03 +0200

    Revert "Arrays Advanced - exercises"
    This reverts commit beda859055e80101b02423ca4afc9530d825ed9e.

--- 5-Arrays Advanced/sumFirstAndLast.js
+++
@@ -1,9 +0,0 @@
-function sumFirstAndLast(array) {
-
-    let firstElement = Number(array[0]);
-    let lastElement = Number(array.pop());
-    
-    let sum = firstElement + lastElement;
-    console.log(sum);
-
-}sumFirstAndLast(['20', '30', '40'])
